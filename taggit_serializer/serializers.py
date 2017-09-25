c948c11152f676e1d0893468d4000a6ec6c9ed63
afk <afk@ellugar.co>
2017-09-25 17:10:26 +0200

    Changed keys to tag_id and tag

--- taggit_serializer/serializers.py
+++ taggit_serializer/serializers.py
@@ -80,7 +80,7 @@ class TagListSerializerField(serializers.Field):
                     tags = value.all().order_by(*self.order_by)
                 else:
                     tags = value.all()
-                value = [{ 'id':tag.id, 'name': tag.name } for tag in tags] 
+                value = [{ 'tag_id':tag.id, 'tag': tag.name } for tag in tags] 
             value = TagList(value, pretty_print=self.pretty_print)
 
         return value
